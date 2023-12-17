f636501cb2d566eedd2f3c7015c55aa4a6fd42cf
Meharwan-Singh2308 <meharwangamers@gmail.com>
2023-12-17 19:50:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Components/header/Header";
+import Footer from "./Components/footer/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import About from "./About/About";
+import Contact from "./contact/Contact";
+import Home from "./Home/Home";
+import User from "./User/User";
+import Github from "./Github/Github";
+
+export { About, Contact, Home , User , Github};
